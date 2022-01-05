a77d88be8f62133625a8f7aa0fd5b8a26da852b5
Axel Pion <pionaxel@gmail.com>
2022-01-05 14:25:09 +0100

    feat(app): kinda counts. pushing work before it breaks

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -8,68 +8,112 @@ class App extends React.Component {
 	constructor(props) {
   	super(props);
     this.state = {
-      choice: null,
+      firstChoice: null,
+      secondChoice: null,
       symbol: null,
-      result: 0
+      result: 0,
+      resultToShow: 0
     }
   }
   
   calculate = () => {
+    console.log(this.state);
     if (this.state.symbol === "+") {
       this.setState({
-        result: this.state.result + parseInt(this.state.choice)
+        result: this.state.firstChoice + this.state.secondChoice,
+        resultToShow: this.state.firstChoice + this.state.secondChoice,
+        symbol: null,
+        firstChoice: null
       }, () => {});
     }
     if (this.state.symbol === "-") {
       this.setState({
-        result: this.state.result - parseInt(this.state.choice)
+        result: this.state.firstChoice - this.state.secondChoice,
+        resultToShow: this.state.firstChoice - this.state.secondChoice,
+        symbol: null
       }, () => {});
     }
     if (this.state.symbol === "x") {
       this.setState({
-        result: this.state.result * parseInt(this.state.choice)
+        result: this.state.firstChoice * this.state.secondChoice,
+        resultToShow: this.state.firstChoice * this.state.secondChoice,
+        symbol: null
       }, () => {});
     }
     if (this.state.symbol === "%") {
       this.setState({
-        result: this.state.result / 100
+        result: this.state.result / 100,
+        resultToShow: this.state.result / 100,
+        symbol: null
       }, () => {});
     }
     if (this.state.symbol === "÷") {
       this.setState({
-        result: this.state.result / parseInt(this.state.choice)
+        result: this.state.result / this.state.choice,
+        resultToShow: this.state.result / this.state.choice,
+        symbol: null
       }, () => {});
     }
     if (this.state.symbol === "±") {
       this.setState({
-        result: this.state.result * -1
+        result: this.state.result * -1,
+        resultToShow: this.state.result * -1,
+        symbol: null
       }, () => {});
     }
   }
 
   onBtnClick = (value) => {
     console.log(value);
-    const regExSymbol = /[\+\-\x\%\÷\±]/;
+    const regExSymbol = /[\+\-\x\÷]/;
     const regExNumbers = /[0-9]/;
+    
+    // BRACE YOURSELF, HERE COMES THE ARMY OF IF !
     if (regExSymbol.test(value) === true) {
-      console.log("true");
       this.setState({
         symbol: value
       }, () => {});
-    }
-    if (regExNumbers.test(value) === true) {
+    } else if (regExNumbers.test(value) === true) {
+      if (this.state.firstChoice === null && this.state.symbol === null) {
+        this.setState({
+          firstChoice: parseInt(value),
+          resultToShow: parseInt(value)
+        }, () => {});
+      } else if (!this.state.choice) {
+        this.setState({
+          secondChoice: parseInt(value),
+          resultToShow: parseInt(value)
+        }, () => {});
+      }
+    } else if (value === "AC") {
       this.setState({
-        choice: value
-      }, () => {});
-    }
-    if (value === "AC") {
-      this.setState({
-        choice: null,
+        firstChoice: null,
+        secondChoice: null,
         symbol: null,
-        result: 0
+        result: 0,
+        resultToShow: 0,
       }, () => {})
+    } else if (value === "%" || value === "±") {
+      if (!this.state.firstChoice) {
+        this.setState({
+          firstChoice: this.state.result,
+        }, () => {});
+      }
+      this.calculate();
+    } else if (value === "=") {
+      if (!this.state.secondChoice && this.state.symbol) {
+        this.setState({
+          firstChoice: this.state.result,
+          secondChoice: this.state.resultToShow,
+        }, () => {})
+        this.calculate();
+      } else if (this.state.secondChoice && this.state.symbol) {
+        this.calculate();
+      }
     }
-    if (this.state.symbol && this.state.choice) {
+
+    console.log(this.state);
+    if (this.state.symbol && this.state.choice && this.state.secondChoice) {
       this.calculate();
     }
   }
@@ -83,8 +127,7 @@ class App extends React.Component {
   	return (
     	<div className="calc">
         <div className="resultDiv">
-          {console.log(this.state.result)}
-    	    <CalcResult result={this.state.result} />
+    	    <CalcResult result={this.state.resultToShow} />
         </div>
         <div className="btnDiv">
           <CalcBtns buttons={btns} onBtnClick={this.onBtnClick.bind(this)} />
